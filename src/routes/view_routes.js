5878f0190305e9d89bed22e2c963ba85ecad20b6
JoaquinOrnelasN <100652211+JoaquinOrnelasN@users.noreply.github.com>
2023-01-02 03:20:23 -0600

    Add files via upload

--- src/routes/view_routes.js
+++ src/routes/view_routes.js
@@ -1,20 +1,20 @@
 import express from "express";
 const router = express.Router();
-import productMananger from "../managers/product_manager.js";
-const productMananger=productMananger
-export let data = await productMananger.getProducts();
+import productManager from "../managers/product_manager.js";
+
+export let data = await productManager.getProducts();
 
 
 router.get(`/`, async (req, res) => {
   res.render(`home`, {
     data,
-    style:"staticList.css"
+    style:"style.css"
   });
 });
 
-router.get(`/realtimeproducts`, async (req, res) => {
-  res.render(`realtimeProducts`, {
-    style:"staticList.css"
+router.get(`/products_realtime`, async (req, res) => {
+  res.render(`products_realtime`, {
+    style:"style.css"
   });
 });
 export default router;
